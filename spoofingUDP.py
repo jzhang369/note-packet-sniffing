e24433ddf8f631f72076957045e4b28402ea4b89
jzhang369 <junjie.gtisc@gmail.com>
2022-08-20 00:33:43 -0400

    added

--- spoofingUDP.py
+++ spoofingUDP.py
@@ -1,8 +1,8 @@
 from scapy.all import *
 
 print("Sending spoofed udp packets......")
-ip = IP(src="1.2.3.4", dst = "8.8.8.8")
-udp = UDP(sport = 8888, dport=53)
+ip = IP(src="1.2.3.4", dst = "127.0.0.1")
+udp = UDP(sport = 8888, dport=9999)
 data = "TTTTTTT\n"
 pkt = ip/udp/data
 pkt.show()
